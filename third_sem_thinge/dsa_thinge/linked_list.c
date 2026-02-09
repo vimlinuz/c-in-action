1c3420c196470000654cd23c790ae11fe371284c
santoshxshrestha <santoshxshrestha@gmail.com>
2026-02-09 16:55:32 +0545

    backupd

--- third_sem_thinge/dsa_thinge/linked_list.c
+++ third_sem_thinge/dsa_thinge/linked_list.c
@@ -1,7 +1,11 @@
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define true 1
-#define false 0
+
+// #define true 1
+// #define false 0
+// typedef int bool;
 
 struct Stack {
   int info;
@@ -9,24 +13,43 @@ struct Stack {
 };
 
 // this will generate new enpty node
-struct Stack *getnode() {
+void get_node(struct Stack *stack, int info) {
   struct Stack *newnode;
   newnode = (struct Stack *)malloc(sizeof(struct Stack));
 
-  // Initialization of the newnode  with some empty content
-  newnode->info = 0;
-  newnode->next = NULL;
-
+  // this is the case of available check
   if (newnode == NULL) {
     printf("Error alocating memory overflow occured");
     exit(1);
   }
-  return newnode;
-};
 
-int is_empty(struct Stack *pointer) { return false; }
+  // Initialization of the newnode  with some empty content
+  newnode->info = info;
+  newnode->next = stack;
+  stack = newnode;
+}
+
+bool is_empty(struct Stack *stack) {
+  if (stack == NULL) {
+    return true;
+  }
+  return false;
+}
+
+int free_node(struct Stack *stack) {
+  if (is_empty(stack)) {
+    printf("Error underflow");
+    exit(1);
+  };
+  int info = stack->info;
+  stack = stack->next;
+  return info;
+}
 
 int main() {
-  printf("this is teh main function ");
-  return 0;
+  struct Stack *stack = (struct Stack *)malloc(sizeof(struct Stack));
+  stack->info = 12;
+  get_node(stack, 43);
+  printf("some tings %d", stack->info);
+  printf("some tings %d", stack->next->info);
 }
