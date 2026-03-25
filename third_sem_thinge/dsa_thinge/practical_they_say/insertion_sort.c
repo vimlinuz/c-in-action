e1edafbe25a362dcba88b033a92a4c98e6d4d0d1
vimlinuz <vimlinuz@gmail.com>
2026-03-25 23:59:45 +0545

    add: insertion sort

--- third_sem_thinge/dsa_thinge/practical_they_say/insertion_sort.c
+++ third_sem_thinge/dsa_thinge/practical_they_say/insertion_sort.c
@@ -1,5 +1,28 @@
-#include <stdio.h>
+#include <assert.h>
+
+void insertion_sort(int arr[], int length) {
+  for (int i = 1; i < length; ++i) {
+    int key = arr[i];
+    int j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+  }
+}
+
 int main() {
-  printf("This is insertion sort\n");
+  int arr[] = {12, 11, 13, 5, 6};
+  int length = sizeof(arr) / sizeof(arr[0]);
+  int sorted[] = {5, 6, 11, 12, 13};
+
+  insertion_sort(arr, length);
+
+  for (int i = 0; i < length; i++) {
+    assert(arr[i] == sorted[i]);
+  }
+
   return 0;
 }
